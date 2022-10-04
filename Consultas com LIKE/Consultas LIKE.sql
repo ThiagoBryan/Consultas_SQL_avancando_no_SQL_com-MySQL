c8b1a5eb3d1e4f9863adbe1852dc5a8a174b9e85
Thiago <tthurler10@gmail.com>
2022-10-04 17:35:04 -0300

    Consultas com LIKE

---
+++ Consultas com LIKE/Consultas LIKE.sql
@@ -0,0 +1,6 @@
+SELECT * FROM tabela_de_produtos WHERE SABOR LIKE '%Maça%';
+
+SELECT * FROM tabela_de_produtos WHERE SABOR = 'Maça';
+
+SELECT * FROM tabela_de_produtos WHERE SABOR LIKE '%Maça%'
+AND EMBALAGEM = 'PET';
